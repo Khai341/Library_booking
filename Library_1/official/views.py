e0131cf24cb64e4185df2573acfd10df2e431da8
hoangp08 <phuc.hoang8@hcmut.edu.vn>
2025-04-04 19:48:34 +0700

    Add files via upload

---
+++ Library_1/official/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Room, Student
+
+# Create your views here.
+def official_list(request):
+    query_room = request.GET.get('q_room', '')
+    query_student = request.GET.get('q_student', '')
+
+    # Filter rooms based on the search query (case insensitive search)
+    rooms = Room.objects.all()
+    if query_room:
+        rooms = Room.objects.filter(id_number__icontains=query_room)
+
+    # Filter students based on the search query (case insensitive search)
+    students = Student.objects.all()
+    if query_student:
+        students = Student.objects.filter(id_number__icontains=query_student)
+
+    # Pass the rooms and query to the template
+    return render(request, 'official/official_list.html',  {'rooms': rooms, 'query_room': query_room, 'students': students, 'query_student': query_student})
