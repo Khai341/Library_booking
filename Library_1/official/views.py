b94ceb62ab75b66cb9085ab42621bb3d510f4c20
hirodesu <153213094+nothiro2k@users.noreply.github.com>
2025-04-16 13:57:52 +0700

    Add files via upload

--- Library_1/official/views.py
+++ Library_1/official/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Room, Student
-from librarianAdmin.models import Users
+
 # Create your views here.
 def official_list(request):
     query_room = request.GET.get('q_room', '')
@@ -12,9 +12,9 @@ def official_list(request):
         rooms = Room.objects.filter(id_number__icontains=query_room)
 
     # Filter students based on the search query (case insensitive search)
-    students = Users.objects.all()
+    students = Student.objects.all()
     if query_student:
-        students = Users.objects.filter(id_number__icontains=query_student)
+        students = Student.objects.filter(id_number__icontains=query_student)
 
     # Pass the rooms and query to the template
     return render(request, 'official/official_list.html',  {'rooms': rooms, 'query_room': query_room, 'students': students, 'query_student': query_student})
