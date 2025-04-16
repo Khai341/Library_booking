1ec541e540d7567a5e022809f9f4f9ee1c9da6fa
Khai341 <khai.tran2004bkhcm@hcmut.edu.vn>
2025-04-16 13:31:46 +0700

    Delete accounts directory

--- accounts/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-#from django.contrib.auth.models import User
-from librarianAdmin.models import Users
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    class Meta:
-        model = Users
-        fields = ['username', 'email', 'password1', 'password2']
