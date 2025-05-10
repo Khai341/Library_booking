cbadb600e524f5c328c06cb2d59b6e7aa941df08
hoangp08 <phuc.hoang8@hcmut.edu.vn>
2025-05-10 12:19:36 +0700

    Add files via upload

---
+++ Library/general/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'general'
+
+urlpatterns = [
+    path('', views.Booking),   #ReturnMain
+    path('Booking', views.Booking),
+    path('Ajax0', views.Ajax0), #, name='ajax-view'
+    path('Ajax1', views.Ajax1),
+    path('Ajax2', views.Ajax2),
+]
